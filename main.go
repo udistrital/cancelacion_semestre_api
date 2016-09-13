9e7d11bd4d786705bcdcfd3f9612c17388e95716
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-09-13 12:14:17 -0500

    Se agrega código inicial de beego

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "github.com/JorgeUlises/oas_be_cancelacion_semestre/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
