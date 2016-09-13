f574f29fb2a3da90ecb521a0d00e248b0b5d00d6
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-09-13 14:16:40 -0500

    Se elimina binario

--- scripts/create_database_project.sh
+++ scripts/create_database_project.sh
@@ -4,3 +4,4 @@ docker run -d -p 5432:5432 -e POSTGRESQL_USER=cancelacion_semestre -e POSTGRESQL
 psql -h localhost -d udistrital -U cancelacion_semestre -W < model/cancelacion_semestre.sql
 #bee api github.com/JorgeUlises/oas_be_cancelacion_semestre -driver=postgres -conn=postgres://cancelacion_semestre:docker@localhost:5432/udistrital?sslmode=disable&search_path=cancelacion_semestre
 #bee run -downdoc=true -gendoc=true
+#URL for swagger: http://127.0.0.1:8080/swagger/
